762fd5743cec43ca30d219a9ae4e154978067d2c
parth <parthajudiya96647@gmail.com>
2021-09-24 09:45:59 +0530

    changes in crud opration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,45 @@
 class ArticlesController < ApplicationController
-   def show
-    @article = Article.find(params[:id])
-   end
-
-   def index
-    @articles = Article.all
-   end
-    
-    def new
-        @article = Article.new
+    def show
+     @article = Article.find(params[:id])
     end
-
-    def edit
-        @article = Article.find(params[:id])
+ 
+    def index
+     @articles = Article.all
     end
-
-    def create
-        @article = Article.new(params.require(:article).permit(:title , :description))  
-       if  @article.save
-        flash[:notice] = "Article created successfully"
-        redirect_to @article
-       else
-        render 'new'
-       end
-    end
-
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title , :description))
-            flash[:notice] = "Article update successfully"
-            redirect_to @article
-       else
-            render 'edit'
-       end
-
-    end
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-        redirect_to articles_path
-    end
-    
-end
+     
+     def new
+         @article = Article.new
+     end
+ 
+     def edit
+         @article = Article.find(params[:id])
+     end
+ 
+     def create
+         @article = Article.new(params.require(:article).permit(:title , :description))  
+        if  @article.save
+         flash[:notice] = "Article created successfully"
+         redirect_to @article
+        else
+         render 'new'
+        end
+     end
+ 
+     def update
+         @article = Article.find(params[:id])
+         if @article.update(params.require(:article).permit(:title , :description))
+             flash[:notice] = "Article update successfully"
+             redirect_to @article
+        else
+             render 'edit'
+        end
+ 
+     end
+     def destroy
+         @article = Article.find(params[:id])
+         @article.destroy
+         redirect_to articles_path
+     end
+     
+ end
+ 
