e07d7df782e470c0584532d6d119034aaef21416
parth <parthajudiya96647@gmail.com>
2021-10-01 11:00:21 +0530

    add edit functinality to the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,31 @@ class UsersController < ApplicationController
 
     def create 
         @user = User.new(params_user)
-
         if @user.save
             flash[:notice] = "you have successfully signed up! #{@user.user_name}"
             redirect_to articles_path
         else
-            render :new
+            render 'new'
+        end
+    end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id]) 
+        if @user.update(params_user)
+            flash[:notice] = "Update successfully "
+            redirect_to articles_path
+        else 
+            render 'edit' 
         end
     end
 
 
     private
     def  params_user
-        params.require(:user).permit(:user_nmae, :email, :password)
+        params.require(:user).permit(:user_name, :email, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'page#home'
   get 'about', to: 'pages#about'
   resources :articles
+
   get "signup", to: "users#new"
-  resource :users
+  resources :users
 end 
