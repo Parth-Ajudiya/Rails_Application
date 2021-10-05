330deae9c20462dc69bb2b6c3e38cb63263967b2
parth <parthajudiya96647@gmail.com>
2021-10-05 09:23:38 +0530

    admin all functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    before_action :require_user, only:[:edit, :update]
+    before_action :require_same_user, only:[:edit, :update]
+     
     def new
         @user = User.new
     end
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
         if @user.save
             session[:user_id] = @user.id
             flash[:notice] = "you have successfully signed up! #{@user.user_name}"
-            redirect_to articles_path
+            redirect_to users_path
         else
             render 'new'
         end
@@ -38,9 +41,23 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        session[user_id] = nil  if @user == current_user
+        flash[:notice] = "Account and your assosiate articles are deleted"
+        redirect_to articles_path
 
     private
     def  params_user
         params.require(:user).permit(:user_name, :email, :password)
     end
+
+    def require_same_user
+        if current_user = @user && !current_user.admin?
+            flash[:alert] = "you can Edit or Delete only own Account"
+            redirect_to @user
+        end 
+    end
+    
 end
